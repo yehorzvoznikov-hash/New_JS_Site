91fd5175220c2947600041bdfc3c874083142790
yehorzvoznikov-hash <yehorzvoznikov@gmail.com>
2026-04-06 13:04:46 +0300

    add first products

---
+++ js/products.js
@@ -0,0 +1,42 @@
+var products = [
+    {
+        id: 1,
+        name: "Товар 1",
+        category: "Категорія 1",
+        price: 1000,
+        emoji: "😊",
+        desc: "Опис товару 1"
+    },
+    {
+        id: 2,
+        name: "Товар 2",
+        category: "Категорія 2",
+        price: 1000,
+        emoji: "😊",
+        desc: "Опис товару 2"
+    },
+    {
+        id: 3,
+        name: "Товар 3",
+        category: "Категорія 3",
+        price: 1000,
+        emoji: "😊",
+        desc: "Опис товару 3"
+    },
+    {
+        id: 4,
+        name: "Товар 4",
+        category: "Категорія 4",
+        price: 1000,
+        emoji: "😊",
+        desc: "Опис товару 4"
+    },
+];
+
+function getProduct(id) {
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === id) return products[i];
+    }
+
+    return null;
+}
